039ce4e9423e4270e79d1972346dcd49cf9cd74d
Bikram Oli <bikramoli@cagtu.com>
2022-05-02 17:22:48 +0545

    first commit

--- apps/demo/pages/_app.tsx
+++ apps/demo/pages/_app.tsx
@@ -1,12 +1,14 @@
 import { AppProps } from 'next/app';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import Head from 'next/head';
 import './styles.css';
+
 
 function CustomApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
-        <title>Welcome to demo!</title>
+        <title>Nx-Demo!</title>
       </Head>
       <main className="app">
         <Component {...pageProps} />
---
+++ apps/demo/pages/about/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import PartyPopper from 'apps/demo/components/party-popper/party-popper';
+
+
+const About = () => {
+  return (
+    <div>
+      <h1 className='text-center bg-primary'>
+        You found the about page! <PartyPopper />
+      </h1>
+    </div>
+  );
+};
+export default About;
